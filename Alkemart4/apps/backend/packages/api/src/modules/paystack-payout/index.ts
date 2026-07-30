7e91cb4fbca67bd183f0318e5497c06a805211e6
Lamptey Odartei Isaiah <isaiahamber5@gmail.com>
2026-07-30 01:41:10 +0000

    fix: add paystack-payout index.ts so module resolves in production build

---
+++ Alkemart4/apps/backend/packages/api/src/modules/paystack-payout/index.ts
@@ -0,0 +1,7 @@
+import PaystackPayoutProvider from "./service"
+import { ModuleProvider } from "@medusajs/framework/utils"
+import { MercurModules } from "@mercurjs/types"
+
+export default ModuleProvider(MercurModules.PAYOUT, {
+  services: [PaystackPayoutProvider],
+})
